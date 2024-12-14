82fe0feb99e02bfca2ba3783d059219fe85c295b
mrzenc <mrzenc@proton.me>
2024-12-14 20:55:40 +0200

    Update nixpkgs input

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "NixOS module which provides NVIDIA vGPU functionality";
 
   inputs = {
-    # a working revision (29/08/2024)
-    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/54fee3a7e34a613aabc6dece34d5b7993183369c.tar.gz";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
